1dde8f2c21bdd80e6094e93dbb09d51f9825417a
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-09-27 03:54:09 -0400

    updated concert this function

--- liri.js
+++ liri.js
@@ -10,29 +10,20 @@ var fs = require('fs');
 var command = process.argv[2];
 var value = process.argv.slice(3).join(" ");
 
-// var userInput = "";
-// for (let i = 3; i < value.length; i++) {
-//     if(i > 3 && i < value.length){
-//         userInput = userInput + "+" + value[i];
-//     }else {
-//         userInput += value[i];
-//     }
-    
-// }
-
 function spotifyThis(input) {
     spotify
-        .search({ type: 'track', query: value, limit : 5})
+        .search({
+            type: 'track',
+            query: value,
+            limit: 5
+        })
         .then(function (resp) {
-                    //    for (let i = 0; i < resp.length; i++) {
-                        
-                    //    } 
-                       console.log("Artist Name: " + JSON.stringify(resp.tracks.items[0].artists[0].name, null, 2));
-                       console.log("Song Title: " + JSON.stringify(resp.tracks.items[0].name, null, 2));
-                       console.log("Preview link: " + JSON.stringify(resp.tracks.items[0].preview_url, null, 2));
-                       console.log("Album Title: " + JSON.stringify(resp.tracks.items[0].album.name, null, 2)); 
-                          
-            
+            console.log("Artist Name: " + JSON.stringify(resp.tracks.items[0].artists[0].name, null, 2));
+            console.log("Song Title: " + JSON.stringify(resp.tracks.items[0].name, null, 2));
+            console.log("Preview link: " + JSON.stringify(resp.tracks.items[0].preview_url, null, 2));
+            console.log("Album Title: " + JSON.stringify(resp.tracks.items[0].album.name, null, 2));
+
+
         })
         .catch(function (err) {
             console.log(err);
@@ -41,22 +32,41 @@ function spotifyThis(input) {
 }
 
 function concertThis() {
-
+    var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
+    var m = moment().format("MM/DD/YYYY")
+    axios.get(queryUrl).then(
+        function (resp) {
+            var data = resp.data
+            for (let i = 0; i < data.length; i++) {
+                console.log('#############################');              
+                console.log("Band/Artist: " + data[i].lineup);
+                console.log("Venue: " + data[i].venue.name);
+                console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region);
+                console.log("Date of Event: " + data[i].datetime);
+                }console.log("##############################")
+        }       
+        
+    )
 }
 
 function movieThis() {
     var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
 
     axios.get(queryUrl).then(
-        function(resp) {
-            console.log(resp);
-            
-          // Then we print out the imdbRating
-          console.log("The movie's Title: " + resp.data.Title);
-          console.log("The movie's was released in: " + resp.data.Year);
-          console.log("The movie's rating is: " + resp.data.imdbRating);
+        function (resp) {
+
+            // Then we print out the imdbRating
+            console.log("The movie's Title: " + resp.data.Title);
+            console.log("The movie's was released in: " + resp.data.Year);
+            console.log("The movie's rating is: " + resp.data.imdbRating);
+            console.log("The movie's Rotten Tomatoes rating is: " + resp.data.Ratings[1].Value);
+            console.log("The movie's was produced in: " + resp.data.Country);
+            console.log("The movie's language: " + resp.data.Language);
+            console.log("The movie's Plot: " + resp.data.Plot);
+            console.log("The movie's Actors: " + resp.data.Actors);
+
         }
-      );
+    );
 }
 
 function doWhatItSays() {
